3e1f1c1a3f0ae5a66199731c2bb159bbbd6461ba
zofiadobrowolska <z.dobrowolska.993@studms.ug.edu.pl>
2025-10-16 15:34:37 +0200

    dodano feature 3

--- src/account.py
+++ src/account.py
@@ -3,4 +3,7 @@ class Account:
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0.0
-        self.pesel = pesel
+        self.pesel = pesel if self.is_pesel_valid(pesel) else 'Invalid'
+
+    def is_pesel_valid(self, pesel):
+        return isinstance(pesel, str) and len(pesel) == 11
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,8 +3,20 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "12345678901")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0.0
         assert account.pesel == "12345678901"
+    
+    def test_pesel_too_long(self):
+        account = Account("Jane", "Doe", "1234567890193245")
+        assert account.pesel == 'Invalid'
+    
+    def test_pesel_too_short(self):
+        account = Account("Jane", "Doe", "123")
+        assert account.pesel == 'Invalid'
+
+    def test_pesel_none(self):
+        account = Account("Jane", "Doe", None)
+        assert account.pesel == 'Invalid'
