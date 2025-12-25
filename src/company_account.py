f2fe5d447e8ae7ab25dac6fe2c9bc29c1a5fd2eb
zofiadobrowolska <z.dobrowolska.993@studms.ug.edu.pl>
2025-12-25 11:49:26 +0100

    part 1

--- src/company_account.py
+++ src/company_account.py
@@ -1,14 +1,44 @@
+import os
+import requests
+from datetime import date
 from src.account import Account
 
 class CompanyAccount(Account):
     def __init__(self, company_name, nip):
         super().__init__() # dzieki temu moge odziedziczyc saldo i obslugiwac przelewy
         self.company_name = company_name
-        self.nip = nip if self.is_nip_valid(nip) else 'Invalid'
+
+        if not self.is_nip_valid(nip):
+            self.nip = 'Invalid'
+        else:
+            if self.check_mf(nip):
+                self.nip = nip
+            else:
+                raise ValueError("Company not registered!!")
 
     def is_nip_valid(self, nip):
         return isinstance(nip, str) and len(nip) == 10 and nip.isdigit()
-    
+
+    def check_mf(self, nip):
+        url_mf = os.getenv('BANK_APP_MF_URL', 'https://wl-test.mf.gov.pl')
+        date = date.today().strftime("%Y-%m-%d")
+        
+        url = f"{url_mf}/api/search/nip/{nip}?date={date}"
+        
+        res = requests.get(url)
+        data = res.json()
+        
+        print(f"MF Response: {data}")
+
+        try:
+            info = data.get("result", {}).get("subject", {})
+            if info and info.get("statusVat") == "Czynny":
+                return True
+        except:
+            pass
+            
+        return False
+        
     def outgoing_express_transfer(self, amount):
         fee = 5.0
         max_overdraft = fee # saldo może zejść poniżej 0 maksymalnie o kwotę opłaty
